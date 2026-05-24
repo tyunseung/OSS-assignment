38389c9f6aaf26ac06ce93d4db8bb073caf6083d
Tae Yunseung <tyunseung@gmail.com>
2026-05-24 18:58:33 +0900

    wine dataset MLP
    MLP를 이용해서 wine dataset 학습
    은닉층 4개 128 -> 64 -> 32 -> 16
    max_iter 500, random_state 7
    ReLU 사용

---
+++ 2545738_OSS과제_태윤승.py
@@ -0,0 +1,28 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+#데이터 불러오기
+wine = datasets.load_wine()
+X = wine.data
+y = wine.target
+
+#분할
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+
+print(X_train.shape)
+print(X_test.shape)
+
+MLP = MLPClassifier(hidden_layer_sizes=(128, 64, 32, 16), activation='relu', max_iter=500, random_state=7)
+
+#학습
+MLP.fit(X_train, y_train)
+
+#예측
+pred = MLP.predict(X_test)
+
+#정답률
+acc = accuracy_score(y_test, pred)
+
+print(acc)
